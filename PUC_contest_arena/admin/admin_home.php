44792805fda0887119cd6b2d7bca1c6c564b7571
mohsin-riad <45259465+mohsin-riad@users.noreply.github.com>
2019-11-17 02:53:24 +0600

    Add files via upload

---
+++ PUC_contest_arena/admin/admin_home.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PUC CONTEST ARENA || HOME</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/print.css" media="print">
+<script src="js/jquery-1.6.4.min.js"></script>
+<script src="js/custom.js"></script>
+
+</head>
+<body>
+<header class="clearFix">
+  <div class="wrap"> <a id="logo" href="#">Puc Contest Arena</a>
+    <hr>
+    <nav>
+      <div id="nav"> <strong>Navigation</strong>
+      <ul>
+          <li> <a href="#">Dashboard</a> </li>
+          <li> <a href="#">Analytics</a> </li>
+          <li> <a href="#">Select</a> </li>
+          <li> <a href="#">Search</a> </li>
+          <li> <a href="#">Tools</a> </li>
+          <li> <a href="#">My Profile</a> </li>
+          <li> <a href="/PUC_contest_arena/login.php">Log Out</a> </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</header>
+<hr>
+<div id="intro">
+  <div class="inner">
+    <div class="wrap clearFix">
+      <h1>Welcome to Administration.<br> <strong>Control your contest and blog.</strong></h1>
+  </div>
+</div>
+<hr>
+<div id="content">
+  <div class="wrap clearFix">
+    <h2>Blogs & Topics</h2>
+    <div class="clearFix">
+      <div class="col floatLeft"> <img src="images/icon-programming.png" alt="" class="icon">
+        <h3><a href="#" class="button">Compete and win</a></h3>
+        <p>Participate in coding contests ranging from beginner level to week-long coding marathons..</p>
+      </div>
+      <div class="col floatRight"> <img src="images/icon-hackathon.png" alt="" class="icon">
+        <h3><a href="#" class="button">Hackathons</a></h3>
+        <p>Solve real-world social problems and business challenges. Win exciting rewards.</p>
+      </div>
+    </div>
+    <div class="clearFix">
+      <div class="col floatLeft"> <img src="images/icon-data.png" alt="" class="icon">
+        <h3><a href="#" class="button">Data Science competitions</a></h3>
+        <p>Analyze and work on a range of high-quality datasets. Put your Data Science skills to test every day.</p>
+      </div>
+      <div class="col floatRight"> <img src="images/icon-tools.png" alt="" class="icon">
+        <h3><a href="#" class="button">Tutorials</a></h3>
+        <p>Explore and add your first skill to get started. PUC  offers a variety of skills, tracks and tutorials for you to learn and improve.</p>
+      </div>
+    </div>
+    
+    <div class="buttonCentered"> <a href="https://www.ideone.com" class="button iconRight">Lets Code & compile <i class="more"></i></a> </div>
+    <h2>WHAT GREAT PROGRAMMERS ARE SAYING</h2>
+    <ul class="cols clearFix">
+      <li>
+        <p>“Talk is cheap. Show me the code.”</p>
+        <p><strong>Linus Torvalds</strong>, Linux</p>
+      </li>
+      <li class="middle">
+        <p>“Programs must be written for people to read, and only incidentally for machines to execute.”</p>
+        <p><strong>Harold Abelson</strong>, Apple</p>
+      </li>
+      <li>
+        <p>Programming is one of the most difficult branches of applied mathematics; the poorer mathematicians had better remain pure mathematicians.</p>
+        <p><strong>Edsger Dijkstra</strong>, Algorithmist</p>
+      </li>
+    </ul>
+  </div>
+</div>
+</div>
+
+<footer class="clearFix">
+  <div class="wrap clearFix">
+    <p class="floatRight"> Copyright &copy; 2019 <a href="https://www.facebook.com/mdmohsin.riad">Mohsin Riad</a> &ndash; All Rights Reserved<br>
+    <p class="socialIcons"> <a href="https://www.facebook.com/mdmohsin.riad" class="facebook">Facebook</a> <a href="https://github.com/mohsin-riad" class="twitter">Github</a> </p>
+  </div>
+</footer>
+</body>
+</html>
+
